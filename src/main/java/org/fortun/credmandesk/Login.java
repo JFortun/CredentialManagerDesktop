3580b8964d828888b4c3b2bf6e7a4d789ef23ab7
JFortun <josemanuelfortunromero@gmail.com>
2021-04-15 20:42:09 +0200

    Basic Login window

--- src/main/java/org/fortun/credmandesk/Login.java
+++ src/main/java/org/fortun/credmandesk/Login.java
@@ -6,10 +6,23 @@ public class Login extends JFrame {
 
     public Login() {
 
-        View.login.setSize(400, 600);
+        View.login.setSize(400, 230);
         View.login.setResizable(false);
+        View.login.setLayout(null);
         View.login.setLocationRelativeTo(null);
         View.login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         View.login.setVisible(true);
+        View.lblLoginNameUser.setBounds(50, 30, 75, 15);
+        View.lblLoginPasswordUser.setBounds(50, 80, 75, 15);
+        View.txtLoginNameUser.setBounds(150, 25, 175, 25);
+        View.txtLoginPasswordUser.setBounds(150, 75, 175, 25);
+        View.btnLoginAccess.setBounds(50,130,95,30);
+        View.btnLoginSignIn.setBounds(235,130,95,30);
+        View.login.add(View.lblLoginNameUser);
+        View.login.add(View.lblLoginPasswordUser);
+        View.login.add(View.txtLoginNameUser);
+        View.login.add(View.txtLoginPasswordUser);
+        View.login.add(View.btnLoginAccess);
+        View.login.add(View.btnLoginSignIn);
     }
 }
--- src/main/java/org/fortun/credmandesk/View.java
+++ src/main/java/org/fortun/credmandesk/View.java
@@ -4,5 +4,12 @@ import javax.swing.*;
 
 public class View {
 
+    // Login
     static JFrame login = new JFrame("Login");
+    static JLabel lblLoginNameUser = new JLabel("User name");
+    static JLabel lblLoginPasswordUser = new JLabel("Password");
+    static JTextField txtLoginNameUser = new JTextField();
+    static JPasswordField txtLoginPasswordUser = new JPasswordField();
+    static JButton btnLoginAccess = new JButton("Access");
+    static JButton btnLoginSignIn = new JButton("Sign In");
 }
