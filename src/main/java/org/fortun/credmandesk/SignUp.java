9d0cd92f301e5ec95772fbbe9061b20db361d7ff
JFortun <josemanuelfortunromero@gmail.com>
2021-04-16 20:21:22 +0200

    Implemented Button action in Sign Up window

--- src/main/java/org/fortun/credmandesk/SignUp.java
+++ src/main/java/org/fortun/credmandesk/SignUp.java
@@ -1,8 +1,10 @@
 package org.fortun.credmandesk;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SignUp extends JFrame {
+public class SignUp extends JFrame implements ActionListener {
 
     public SignUp() {
 
@@ -20,6 +22,8 @@ public class SignUp extends JFrame {
         View.txtSignUpPasswordUserAgain.setBounds(175, 125, 150, 25);
         View.btnSignUp.setBounds(50, 175, 95, 30);
         View.btnSignUpCancel.setBounds(235, 175, 95, 30);
+        View.btnSignUp.addActionListener(this);
+        View.btnSignUpCancel.addActionListener(this);
         View.signUp.add(View.lblSignUpNameUser);
         View.signUp.add(View.lblSignUpPasswordUser);
         View.signUp.add(View.lblSignUpPasswordUserAgain);
@@ -28,5 +32,16 @@ public class SignUp extends JFrame {
         View.signUp.add(View.txtSignUpPasswordUserAgain);
         View.signUp.add(View.btnSignUp);
         View.signUp.add(View.btnSignUpCancel);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        if (actionEvent.getSource().equals(View.btnSignUp)) {
+            System.out.println("Sign Up");
+        } else if (actionEvent.getSource().equals(View.btnSignUpCancel)) {
+            System.out.println("Cancel");
+            View.btnSignUpCancel.removeActionListener(this);
+            View.signUp.dispose();
+        }
     }
 }
