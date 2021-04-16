60807f789b4d8b6b715ffc1c8dee748377169801
JFortun <josemanuelfortunromero@gmail.com>
2021-04-16 21:04:01 +0200

    Implemented Button action in Credential Manager window

--- src/main/java/org/fortun/credmandesk/CredentialManager.java
+++ src/main/java/org/fortun/credmandesk/CredentialManager.java
@@ -1,8 +1,10 @@
 package org.fortun.credmandesk;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class CredentialManager extends JFrame {
+public class CredentialManager extends JFrame implements ActionListener {
 
     public CredentialManager() {
 
@@ -14,7 +16,20 @@ public class CredentialManager extends JFrame {
         View.credentialManager.setVisible(true);
         View.btnCredentialManagerManageUser.setBounds(550, 50, 120, 30);
         View.btnCredentialManagerSignOut.setBounds(50, 50, 95, 30);
+        View.btnCredentialManagerManageUser.addActionListener(this);
+        View.btnCredentialManagerSignOut.addActionListener(this);
         View.credentialManager.add(View.btnCredentialManagerManageUser);
         View.credentialManager.add(View.btnCredentialManagerSignOut);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        if (actionEvent.getSource().equals(View.btnCredentialManagerManageUser)) {
+            System.out.println("Manage User");
+        } else if (actionEvent.getSource().equals(View.btnCredentialManagerSignOut)) {
+            System.out.println("Sign Out");
+            View.btnCredentialManagerSignOut.removeActionListener(this);
+            View.credentialManager.dispose();
+        }
     }
 }
